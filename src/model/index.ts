513fc901a4c42378fda3cdc37dde4d1cf05ef09b
a829573 <anthony.undang@gmail.com>
2020-04-01 16:00:24 +0800

    refactor: removing todos

--- src/model/index.ts
+++ src/model/index.ts
@@ -1,8 +1,5 @@
-import { TodoAction } from './todo';
 import { EmployeeAction } from './employee';
-
-export * from './todo';
 
 export * from './employee';
 
-export type Action = TodoAction | EmployeeAction;
+export type Action = EmployeeAction;
--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,16 +1,13 @@
 import { History } from 'history';
 import { combineReducers } from 'redux';
-import { Todo, IEmployee } from '../model';
-import * as todoReducer from './todo';
+import { IEmployee } from '../model';
 import * as employeeReducer from './employee';
 
 export interface RootState {
-  todoList: Todo[];
   employeeList: IEmployee[];
 }
 
 export default (history: History) =>
   combineReducers({
-    ...todoReducer,
     ...employeeReducer
   });
